1ea852e34dfba59ca53a1ca5a36a7c6f4e3ee79b
Ade Ismail Siregar <adeismailbox@gmail.com>
2023-05-14 16:15:34 +0700

    Create custom_collection_statistic.plugin.php

---
+++ custom_collection_statistic.plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Custom Collection Statistic
+ * Plugin URI: https://github.com/drajathasan/slims-buku-induk
+ * Description: Collection Statistic with filter date popular titles
+ * Version: 1.0.0
+* Author: original creator by Erwan Setyo Budi & plugin by Ade Ismail
+ * Author URI: https://github.com/adeism/slims_custom_statistik_koleksi
+ */
+
+// get plugin instance
+$plugin = \SLiMS\Plugins::getInstance();
+
+// registering menus or hook
+$plugin->registerMenu("reporting", "Custom Collection Statistic", __DIR__ . "/index.php");
